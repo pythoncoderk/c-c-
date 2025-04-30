523d7da4ff8f1949c79eb917403bf41d36ae99fc
Python_coder_K <turntabroz@gmail.com>
2025-05-01 06:51:23 +0900

    paiza

---
+++ 0501_6.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a + b < c + d) {
+        cout << a + b << endl;
+    }
+    else {
+        cout << c + d << endl;
+    }
+}
---
+++ 0501_7.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+    char i;
+    cin >> i;
+    int b;
+    cin >> b;
+    if (a[b-1] == i) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+}
