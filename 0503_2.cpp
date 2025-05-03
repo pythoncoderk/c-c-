1657d415783044f47d7af92c5c654be5bb6a0c69
Python_coder_K <turntabroz@gmail.com>
2025-05-03 20:28:06 +0900

    paiza

---
+++ 0503_2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n >= 1000) {
+        cout << n / 10 << endl;
+    }
+    else {
+        cout << 0 << endl;
+    }
+}
---
+++ 0503_4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    std::cout << (double)n * 1.5 << std::endl;
+
+    }
