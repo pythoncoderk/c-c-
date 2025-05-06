5aa43c6c704d8e83fd6f15e3142feefd50b1c056
Python_coder_K <turntabroz@gmail.com>
2025-05-07 06:47:43 +0900

    paiza

---
+++ 0507_5.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, a, b, c;
+    cin >> n >> a >> b >> c;
+    if (n >= a) {
+        cout << n * b << endl;
+    }
+    else {
+        cout << n * c << endl;
+    }
+}
