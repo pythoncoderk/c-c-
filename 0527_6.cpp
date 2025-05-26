aeb6059b2d1c5879cb044255ce6e5f8571fb0a31
Python_coder_K <turntabroz@gmail.com>
2025-05-27 07:01:13 +0900

    atcoder

---
+++ 0527_6.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double a, b;
+    cin >> a >> b;
+    double x = (((a + b) + 2) - 1) / 2;
+    int ans = x;
+    cout << ans << endl;
+}
