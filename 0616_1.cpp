c788c082b9ab9a96eb88c1dc4d007510c78b8258
Python_coder_K <turntabroz@gmail.com>
2025-06-17 07:44:07 +0900

    atcoder

--- 0616_1.cpp
+++ 0616_1.cpp
@@ -1,3 +1,22 @@
-//
-// Created by turnt on 25/06/16.
-//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int ans = 0;
+    for (int i = a; i <= b; i++) {
+        string s1;
+        s1 = to_string(i);
+        reverse(s1.begin(), s1.end());
+        if (s1 == to_string(i)) {
+            ans++;
+        }
+
+
+    }
+    cout << ans << endl;
+
+
+
+}
