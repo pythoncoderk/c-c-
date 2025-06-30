160332ad955bdcc41716ef603f05d4d9f869b673
Python_coder_K <turntabroz@gmail.com>
2025-07-01 07:29:32 +0900

    paiza

---
+++ 0701_5.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    if (s1 == s2) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+}
