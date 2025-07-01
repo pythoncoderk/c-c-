66a42643d0b8732cb20a044bd3089d41bc64d400
Python_coder_K <turntabroz@gmail.com>
2025-07-02 07:28:54 +0900

    paiza

---
+++ 0702_17.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a >= 20 && b >= 20 && c >= 20) {
+        cout << "OK" << endl;
+    }
+    else {
+        cout << "NG" << endl;
+    }
+}
