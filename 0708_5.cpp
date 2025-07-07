70e05007c4d25d0f71bebf7581e693e8c595f4e9
Python_coder_K <turntabroz@gmail.com>
2025-07-08 07:32:30 +0900

    algo

---
+++ 0708_5.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    if (n > m) {
+        cout << "No" << endl;
+    }
+    else {
+        cout << "Yes" << endl;
+    }
+}
---
+++ 0708_7.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    char c1 = s[0];
+    char c2 = s[s.size()-1];
+    cout << c1 << c2 << endl;
+}
