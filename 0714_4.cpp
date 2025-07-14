4aad0e6bd2b84f3c56370bf8def9c0ffb9301245
Python_coder_K <turntabroz@gmail.com>
2025-07-14 23:53:21 +0900

    algo

---
+++ 0714_4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float n;
+    cin >> n;
+    if (n >= 37.0) {
+        cout << "NG" << endl;
+    }
+    else {
+        cout << "OK" << endl;
+    }
+}
