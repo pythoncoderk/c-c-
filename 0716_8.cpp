4567af1f1f1881aada573616c90c8ae86d889595
Python_coder_K <turntabroz@gmail.com>
2025-07-16 07:16:54 +0900

    algo

---
+++ 0716_8.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n >= 1000) {
+        cout << n + 3 << endl;
+    }
+    else {
+        cout << n << endl;
+    }
+}
