8af58b063943554b0ca6b3efa744eddeaacd837c
Python_coder_K <turntabroz@gmail.com>
2025-07-23 07:04:08 +0900

    algo

--- 0722_9.cpp
+++ 0722_9.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-
+int main(){
+    string s;
+    int n;
+    cin >> s >> n;
+    int total = 0;
+    for (int i = 0; i < s.size(); i++) {
+        char c = s[i];
+        if (c == 'R') {
+            total++;
+        }
+    }
+    if (total >= n) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
 }
