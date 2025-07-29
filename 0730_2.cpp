e83c1c7b2fab6f1e0a4f4734d7e5ff71ae7ae78c
Python_coder_K <turntabroz@gmail.com>
2025-07-30 06:38:41 +0900

    algo

---
+++ 0730_2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    if (s2 == "F") {
+        cout << "Hi, Ms. " << s1 << endl;
+    }
+    else {
+        cout << "Hi, Mr. " << s1 << endl;
+    }
+}
