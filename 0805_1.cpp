49dfa3edbc8d126bb7550a295e23b4db75c6f7dc
Python_coder_K <turntabroz@gmail.com>
2025-08-05 07:32:35 +0900

    paiza

---
+++ 0805_1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 0;
+    n += 3286;
+    n *= 4736;
+    n %= 12312;
+    cout << n << endl;
+}
---
+++ 0805_7.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string n, m;
+    cin >> n >> m;
+    cout << n << endl;
+    cout << m << endl;
+}
