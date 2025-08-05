5cd5102979fc008e97ee54f91be659b55f8a6e1d
Python_coder_K <turntabroz@gmail.com>
2025-08-06 06:21:23 +0900

    paiza

---
+++ 0806_3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v(5);
+    for (int i = 0; i < 5; i++) {
+        cin >> v[i];
+    }
+    cout << *min_element(v.begin(), v.end()) << endl;
+}
