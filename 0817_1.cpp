54f46f7e236e4c1d142ac5705742846d5aa3ec9e
Python_coder_K <turntabroz@gmail.com>
2025-08-17 08:48:00 +0900

    atcoder

---
+++ 0817_1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n >= 200 && n <= 299) {
+        cout << "Success" << endl;
+    }
+    else {
+        cout << "Failure" << endl;
+    }
+}
