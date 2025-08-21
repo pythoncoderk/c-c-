bef2999cf5e53ebd503908984b25c7b7d226ab93
Python_coder_K <turntabroz@gmail.com>
2025-08-22 06:37:43 +0900

    atcoder

---
+++ 0822_2.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float a = 0.5;
+    cout << a * 100 * (100 + 1) << endl;
+}
---
+++ 0822_3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int seconds = 365 * 24 * 60 * 60;
+    cout << (365 * 24 * 60 * 60) << endl;
+    cout << (365 * 24 * 60 * 60) * 2 << endl;
+    cout << (365 * 24 * 60 * 60) * 5 << endl;
+    cout << (365 * 24 * 60 * 60) * 10 << endl;
+}
