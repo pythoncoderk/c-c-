ee0ba031567d3205403544cb6e7c90814d057851
Python_coder_K <turntabroz@gmail.com>
2025-08-30 08:44:14 +0900

    atcoder

---
+++ 0830_1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double n, m;
+    cin >> n >> m;
+    cout << round(n / m) << endl;
+}
+
+//test
---
+++ 0830_2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a > c) {
+        cout << "Yes" << endl;
+    }
+    else if (a == c) {
+        if (b >= d) {
+            cout << "Yes" << endl;
+        }
+        else {
+            cout << "No" << endl;
+        }
+    }
+    else {
+        cout << "No" << endl;
+    }
+}
