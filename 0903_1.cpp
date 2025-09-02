c09a1e17be849d1a810f2220b71f0dbf674fa6fa
Python_coder_K <turntabroz@gmail.com>
2025-09-03 06:41:59 +0900

    atcoder

---
+++ 0903_1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if ((i+1) % 2 != 0) {
+            sum += x;
+        }
+    }
+    cout << sum << endl;
+}
---
+++ 0903_2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n % 2 == 0) {
+        for (int i = 0; i < (n / 2); i++) {
+            cout << "-";
+        }
+        cout << "==";
+        for (int i = 0; i < (n / 2); i++) {
+            cout << "-";
+        }
+    }
+    else {
+        for (int i = 0; i < (n / 2); i++) {
+            cout << "-";
+        }
+        cout << "=";
+        for (int i = 0; i < (n / 2); i++) {
+            cout << "-";
+        }
+    }
+    cout << endl;
+}
