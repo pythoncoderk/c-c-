36969983a69e4c925225adea52e1a7f2fd6fcb6b
Python_coder_K <turntabroz@gmail.com>
2025-09-08 06:34:42 +0900

    atcoder

---
+++ 0908_2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    vector<int> v = {a, b, c};
+    sort(v.begin(), v.end());
+    if (v[0] * v[1] == v[2]) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+}
