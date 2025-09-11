f5d2188ca110023631cd4f4ce9fad24f88f0a53f
Python_coder_K <turntabroz@gmail.com>
2025-09-12 07:07:08 +0900

    atcoder

---
+++ 0911_3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int x = int(s[0]-'0');
+    int y = int(s[2]-'0');
+    cout << x * y << endl;
+
+
+}
