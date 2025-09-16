19982e6aea2c8e823cf94b7b7bd980bf125fa5a0
Python_coder_K <turntabroz@gmail.com>
2025-09-17 08:15:57 +0900

    atcoder

--- 0916_2.cpp
+++ 0916_2.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    int count = 0;
+    int l = 0;
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+
+        if (i != 0) {
+            l -= x - count;
+        }
+        if (l < 0) {
+            l = 0;
+        }
+        l += y;
+        count = x;
+    }
+    cout << l << endl;
 }
