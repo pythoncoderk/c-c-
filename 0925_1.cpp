37390d0e309ddf5be79fb2853e2650e0d01599ad
Python_coder_K <turntabroz@gmail.com>
2025-09-25 06:41:33 +0900

    atcoder

--- 0925_1.cpp
+++ 0925_1.cpp
@@ -7,8 +7,8 @@ int main() {
     string s;
     cin >> s;
     int count = 0;
-    for (int i = 0; i < n; i++) {
-        if (s[i] == '.') {
+    for (int i = 0; i < n-2; i++) {
+        if (s[i] == '#' && s[i+2] == '#' && s[i+1] == '.') {
             count++;
         }
     }
