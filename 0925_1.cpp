3cd3c25d3ea9a5129694a3828b95f16dfe52e635
Python_coder_K <turntabroz@gmail.com>
2025-09-25 06:38:15 +0900

    atcoder

---
+++ 0925_1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '.') {
+            count++;
+        }
+    }
+    cout << count << endl;
+}
