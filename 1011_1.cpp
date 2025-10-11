17eac84b882ccb697e516d3f3f02462e27c60061
Python_coder_K <turntabroz@gmail.com>
2025-10-11 15:46:51 +0900

    atcoder

---
+++ 1011_1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    if (s == "RSM") {
+        cout << "Yes" << endl;
+    }
+    else if (s == "RMS") {
+        cout << "Yes" << endl;
+    }
+    else if (s == "SRM") {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+}
