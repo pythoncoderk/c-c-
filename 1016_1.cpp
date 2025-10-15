efdc03be7defb7839a41b54096be3915da7e66cb
pythoncoderk <turntabroz@gmail.com>
2025-10-16 07:17:25 +0900

    atcoder

---
+++ 1016_1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, c;
+    cin >> x >> c;
+
+    int cnt = x / (1000 + c);
+    int ans = cnt * 1000;
+
+    cout << ans << "\n";
+    return 0;
+}
