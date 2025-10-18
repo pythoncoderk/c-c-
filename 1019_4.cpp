8fe65afcd9620b920e2655235c2ac9dd3461f0d1
pythoncoderk <turntabroz@gmail.com>
2025-10-19 08:18:27 +0900

    atcoder

---
+++ 1019_4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    if ((n * m) % 2 == 0) {
+        cout << "Even" << endl;
+    }
+    else {
+        cout << "Odd" << endl;
+    }
+}
