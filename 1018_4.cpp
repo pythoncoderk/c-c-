058fe85582a1d4779617ea3140cba6f1801d4506
pythoncoderk <turntabroz@gmail.com>
2025-10-18 23:10:55 +0900

    atcoder c++

---
+++ 1018_4.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "a";
+    cout << "b" << endl;
+    cout << "C" << "D" << endl;
+}
---
+++ 1018_6.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello, world!" << endl; //Hello, worldと表示
+    /*
+     これはなかなか便利ですねぇ～
+     */
+}
