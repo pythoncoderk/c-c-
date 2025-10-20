253e114846911be977e972ce9baa5133c268c0d2
pythoncoderk <turntabroz@gmail.com>
2025-10-21 06:41:19 +0900

    atcoder

---
+++ 1021_1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int a_i = 0, b_i = 0;
+    cout << "A:";
+    while (a > 0) {
+        cout << "]";
+        a--;
+    }
+    cout << endl;
+    cout << "B:";
+    while (b > 0) {
+        cout << "]";
+        b--;
+    }
+    cout << endl;
+
+
+}
