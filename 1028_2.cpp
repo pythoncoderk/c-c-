8b38a14ab1ab2a135dfeb0976ca730a7be5f8cf4
pythoncoderk <turntabroz@gmail.com>
2025-10-28 07:06:52 +0900

    atcoder

---
+++ 1028_2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    string c;
+    cin >> a >> c >> b;
+    if (c == "+") cout << a + b << endl;
+    else cout << a - b << endl;
+
+}
