92cf5dc20e9a3a1929474288a15b98c365449ecc
pythoncoderk <turntabroz@gmail.com>
2025-11-06 06:48:17 +0900

    atcoder

---
+++ 1106_2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string n;
+    cin >> n;
+    if (n == "A") cout << 1 << endl;
+    else if (n == "B") cout << 2 << endl;
+    else if (n == "C") cout << 3 << endl;
+    else if (n == "D") cout << 4 << endl;
+    else if (n == "E") cout << 5 << endl;
+}
