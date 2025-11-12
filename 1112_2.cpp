249ddc02850d1e9f1ecd5396bf6dd1612f002be7
pythoncoderk <turntabroz@gmail.com>
2025-11-13 06:52:27 +0900

    atcoder

---
+++ 1112_2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    for (int i = 0; i < n - 1; i++) {
+        cout << v[i] * v[i + 1];
+        if (i == n - 1) cout << endl;
+        else cout << " ";
+    }
+}
