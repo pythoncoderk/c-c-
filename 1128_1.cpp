05293ebca05a22f5deaa2d8f43d67b84f23154c5
pythoncoderk <turntabroz@gmail.com>
2025-11-28 07:39:58 +0900

    atcoder

---
+++ 1128_1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int paiza_age = 15;
+    int paiza_kg = 60;
+
+    if (paiza_age >= n && paiza_kg <= m) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
