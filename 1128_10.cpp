4f514b2e64056d85b6d299ce22834be42acad5fb
pythoncoderk <turntabroz@gmail.com>
2025-11-29 00:57:28 +0900

    atcoder

---
+++ 1128_10.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    if (s[s.size() - 1] == 'd') cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
