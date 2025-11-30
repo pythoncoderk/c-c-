5a7da093eba7dd3a0e2ce89c2796f9c021bdea7c
pythoncoderk <turntabroz@gmail.com>
2025-12-01 07:30:26 +0900

    atcode

---
+++ 1201_5.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    vector<int> v = {a, b, c};
+    cout << accumulate(v.begin(), v.end(), 0) << endl;
+}
