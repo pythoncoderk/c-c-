16110a88b716de7a74768abc7a0a12bab5aadccb
pythoncoderk <turntabroz@gmail.com>
2025-12-04 07:19:53 +0900

    atcoder

---
+++ 1204_5.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1;
+    char s2;
+    int n;
+    cin >> s1 >> s2 >> n;
+    if (s1[n-1] == s2) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+}
