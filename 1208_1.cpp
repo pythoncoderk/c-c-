139efa21e2829f1426c8cc3712491c6d3a0d8921
pythoncoderk <turntabroz@gmail.com>
2025-12-08 21:50:54 +0900

    atcoder

---
+++ 1208_1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    vector<char> v;
+    for (int i = 0; i < s.size(); i++) {
+        v.push_back(s[i]);
+    }
+    set<char> c(v.begin(), v.end());
+    if (c.size() <= 3) cout << "NG" << endl;
+    else cout << "OK" << endl;
+}
---
+++ 1208_2.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n >= 1000) cout << n + 3 << endl;
+    else cout << n << endl;
+}
