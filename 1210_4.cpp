c6646bd4bb56ae7f9f15a8827ba1916363e6ef56
pythoncoderk <turntabroz@gmail.com>
2025-12-10 21:37:42 +0900

    atcoder

---
+++ 1210_4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    int n;
+    cin >> s >> n;
+    if (s == "chocolate") cout << n * 2 << endl;
+    else cout << n * 5 << endl;
+}
